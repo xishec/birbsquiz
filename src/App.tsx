a6ccc3f6aac47fca4244ddbee5faf6acd44beef7
xicshen <xicshen@expediagroup.com>
2023-06-05 01:28:12 -0400

    V3.0 den to test

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import React, { useCallback, useEffect, useMemo } from "react";
 import Button from "@mui/material/Button";
 import Autocomplete from "@mui/material/Autocomplete";
 import TextField from "@mui/material/TextField";
-import rawBirbs from "./birbsMap.json";
+import rawBirbsMapFr from "./dendroica/birbsMapFr.json";
+import rawRecordingsMap from "./dendroica/recordingsMap.json";
 import {
   Box,
   Chip,
@@ -35,7 +36,7 @@ function LinearProgressWithLabel(
   );
 }
 
-const birdEmojis = [
+const birbEmojis = [
   " 🐦‍⬛",
   " 🦤",
   " 🦜",
@@ -52,101 +53,95 @@ const birdEmojis = [
 ];
 
 function App() {
-  let birbsMap = rawBirbs as any;
+  const birbsMapFr = rawBirbsMapFr as any;
+  const recordingsMap = rawRecordingsMap as any;
+
   const [birbInput, setBirbInput] = React.useState<string>("");
-  const [selectedBirb, setSelectedBirb] = React.useState<string>("");
-  const [selectedBirbs, setSelectedBirbs] = React.useState<Array<string>>(
+  const [selectedBirbId, setSelectedBirbId] = React.useState<string>("");
+  const [selectedBirbIds, setSelectedBirbIds] = React.useState<Array<string>>(
     () => {
       const urlBirbs = new URLSearchParams(window.location.search).get("birbs");
       if (urlBirbs)
-        return JSON.parse(urlBirbs).filter((birb: any) => birbsMap[birb]);
+        return JSON.parse(urlBirbs).filter((birbId: any) => birbsMapFr[birbId]);
 
-      const localStorageBirbs = localStorage.getItem("selectedBirbs");
+      const localStorageBirbs = localStorage.getItem("selectedBirbIds");
       if (localStorageBirbs)
         return JSON.parse(localStorageBirbs).filter(
-          (birb: any) => birbsMap[birb]
+          (birbId: any) => birbsMapFr[birbId]
         );
 
       return [];
     }
   );
-  const [audioSources, setAudioSources] = React.useState<
-    Map<string, Array<string>>
-  >(new Map());
+  // const [audioSources, setAudioSources] = React.useState<
+  //   Map<string, Array<string>>
+  // >(new Map());
   const [counter, setCounter] = React.useState(0);
   const [quizStarted, setQuizStarted] = React.useState(false);
   const [showAnswer, setShowAnswer] = React.useState(false);
   const [sequence, setSequence] = React.useState<Array<number>>();
   const [openSnake, setOpenSnake] = React.useState(false);
   const [snakeMessage, setSnakeMessage] = React.useState("");
-  const birdEmoji = useMemo(
-    () => birdEmojis[Math.floor(Math.random() * birdEmojis.length)],
+  const birbEmoji = useMemo(
+    () => birbEmojis[Math.floor(Math.random() * birbEmojis.length)],
     []
   );
 
   const addBirb = useCallback(
-    (birbToAdd: string) => {
-      if (
-        birbsMap[birbToAdd] &&
-        !selectedBirbs.find((birb) => birb === birbToAdd)
-      ) {
-        setSelectedBirbs([...selectedBirbs, birbToAdd]);
+    (birbId: string) => {
+      if (birbsMapFr[birbId] && !selectedBirbIds.find((id) => id === birbId)) {
+        setSelectedBirbIds([...selectedBirbIds, birbId]);
       }
       setBirbInput("");
-      setSelectedBirb("");
+      setSelectedBirbId("");
     },
-    [birbsMap, selectedBirbs]
+    [birbsMapFr, selectedBirbIds]
   );
 
   const deleteBirb = useCallback(
-    (birbToDelete: string) => {
-      const newSelectedBirbs = selectedBirbs?.filter(
-        (birb) => birb !== birbToDelete
-      );
-      setSelectedBirbs(newSelectedBirbs!);
+    (birbId: string) => {
+      const newSelectedBirbIds = selectedBirbIds?.filter((id) => id !== birbId);
+      setSelectedBirbIds(newSelectedBirbIds!);
     },
-    [selectedBirbs]
+    [selectedBirbIds]
   );
 
-  const fetchBirb = useCallback(
-    (birb: string) => {
-      if (!birbsMap[birb]) {
-        deleteBirb(birb);
-      }
-      const latinName = birbsMap[birb] as string;
-      fetch(
-        `https://xeno-canto.org/api/2/recordings?query=${latinName
-          .replace(" ", "%20")
-          .toLowerCase()}%20q:A`
-      )
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.recordings.length > 0) {
-            const recordings = data.recordings
-              .filter(
-                (recording: any) =>
-                  !recording["file-name"].includes(".wav") &&
-                  !recording.type.includes("drumming")
-              )
-              .splice(0, 3)
-              .map((recording: any) => recording.file);
-            setAudioSources(new Map(audioSources?.set(birb, recordings)));
-          } else {
-            setAudioSources(new Map(audioSources?.set(birb, [])));
-          }
-        });
-    },
-    [audioSources, birbsMap, deleteBirb]
-  );
+  // const fetchBirb = useCallback(
+  //   (birbId: string) => {
+  //     console.log("fetchBirb", birbsMapFr[birbId]);
+  //     if (!birbsMapFr[birbId]) {
+  //       deleteBirb(birbId);
+  //     } else {
+  //       fetch(
+  //         `https://www.whateverorigin.org/get?url=https://www.natureinstruct.org/srv/json.php/get_species/${birbId}`
+  //       )
+  //         .then((response) => response.json())
+  //         .then((data) => {
+  //           const regex = /\/files(.*?)mp3/g;
+  //           const found = data.contents.match(regex);
+  //           if (found.length > 0) {
+  //             const recordings = found
+  //               .splice(0, 3)
+  //               .map((path: any) => `https://www.natureinstruct.org${path}`);
+  //             setAudioSources(new Map(audioSources?.set(birbId, recordings)));
+  //           } else {
+  //             setAudioSources(new Map(audioSources?.set(birbId, [])));
+  //           }
+  //         })
+  //         .catch((e) => console.warn(e));
+  //     }
+  //   },
+  //   [audioSources, birbsMapFr, deleteBirb]
+  // );
 
   const startQuiz = () => {
     setQuizStarted(true);
     setCounter(0);
-    randomSequence(selectedBirbs.length);
+    randomSequence(selectedBirbIds.length);
   };
 
   const nextQuestion = () => {
-    if (counter === selectedBirbs.length - 1) {
+    if (counter === selectedBirbIds.length - 1) {
       endQuiz();
     } else {
       setCounter(counter + 1);
@@ -162,32 +157,67 @@ function App() {
   const copyUrl = () => {
     let url = `${
       window.location.href.split("?")[0]
-    }?birbs=${localStorage.getItem("selectedBirbs")}`.replaceAll(" ", "%20");
+    }?birbs=${localStorage.getItem("selectedBirbIds")}`.replaceAll(" ", "%20");
     navigator.clipboard.writeText(url);
     setSnakeMessage("Lien copié!");
     setOpenSnake(true);
   };
 
-  useEffect(() => {
-    if (selectedBirb) addBirb(selectedBirb);
-  }, [selectedBirb, addBirb]);
+  const hi = async () => {
+    const idsToFetch = Object.keys(birbsMapFr);
+    const myMap: any = {};
+
+    // while (myMap.size < idsToFetch.length) {
+    for (const birbId of idsToFetch) {
+      if (myMap[birbId]) continue;
+      fetch(`https://www.natureinstruct.org/srv/json.php/get_species/${birbId}`)
+        .then((response) => response.text())
+        .then((data) => {
+          const regex = /\/files(.*?)mp3/g;
+          const found = data.match(regex);
+          if (found && found.length > 0) {
+            const recordings = found
+              .splice(0, 3)
+              .map((path: any) => `https://www.natureinstruct.org${path}`);
+            myMap[birbId] = recordings;
+            console.log(JSON.stringify(myMap));
+          } else {
+            myMap[birbId] = [];
+          }
+        })
+        .catch((e) => console.warn(e));
+    }
+  };
 
   useEffect(() => {
-    localStorage.setItem("selectedBirbs", JSON.stringify(selectedBirbs));
-  }, [selectedBirbs]);
+    // hi();
+  }, []);
+
+  // useEffect(() => {
+  //   console.log(audioSources);
+  // }, [audioSources]);
 
   useEffect(() => {
-    if (
-      !quizStarted ||
-      !counter ||
-      !sequence ||
-      counter >= selectedBirbs.length - 1
-    )
-      return;
+    if (selectedBirbId) addBirb(selectedBirbId);
+  }, [selectedBirbId, addBirb]);
 
-    // fetch next bird
-    fetchBirb(selectedBirbs[sequence![counter + 1]]);
-  }, [quizStarted, counter, sequence, fetchBirb, selectedBirbs]);
+  useEffect(() => {
+    localStorage.setItem("selectedBirbIds", JSON.stringify(selectedBirbIds));
+  }, [selectedBirbIds]);
+
+  // useEffect(() => {
+  //   if (!quizStarted || !sequence) return;
+
+  //   // fetch this birb
+  //   if (counter === 0) {
+  //     fetchBirb(selectedBirbIds[sequence![counter]]);
+  //   }
+
+  //   // fetch next birb
+  //   if (counter < selectedBirbIds.length - 1) {
+  //     fetchBirb(selectedBirbIds[sequence![counter + 1]]);
+  //   }
+  // }, [quizStarted, counter, sequence, fetchBirb, selectedBirbIds]);
 
   const randomSequence = (max: number) => {
     const newSequence = [...Array(max).keys()];
@@ -205,7 +235,8 @@ function App() {
   };
 
   const getAudioSource = () => {
-    const audioSource = audioSources.get(selectedBirbs[sequence![counter]]);
+    // const audioSource = audioSources.get(selectedBirbIds[sequence![counter]]);
+    const audioSource = recordingsMap[selectedBirbIds[sequence![counter]]];
     return (
       <>
         {!audioSource && (
@@ -233,7 +264,7 @@ function App() {
           ))}
 
         {audioSource && audioSource.length === 0 && (
-          <Typography variant="body1">This brib doesn't sing</Typography>
+          <Typography variant="body1">Cet oiseau ne change pas.</Typography>
         )}
       </>
     );
@@ -263,7 +294,7 @@ function App() {
             {/* <Box component="span" sx={{ color: "primary.main" }}> */}
             Birbsquiz
             {/* </Box> */}
-            {birdEmoji}
+            {birbEmoji}
           </Typography>
 
           {!quizStarted && (
@@ -271,10 +302,18 @@ function App() {
               <Autocomplete
                 inputValue={birbInput}
                 onInputChange={(e, v) => setBirbInput(v)}
-                value={selectedBirb}
-                onChange={(e, v) => setSelectedBirb(v!)}
-                options={Object.keys(birbsMap)}
-                getOptionLabel={(option) => option}
+                value={selectedBirbId}
+                onChange={(e, v) => setSelectedBirbId(v!)}
+                options={Object.keys(birbsMapFr).sort((a, b) =>
+                  birbsMapFr[a].localeCompare(birbsMapFr[b])
+                )}
+                getOptionLabel={(birbId) =>
+                  birbsMapFr[birbId] ? birbsMapFr[birbId] : ""
+                }
+                freeSolo
+                isOptionEqualToValue={(birbId, input) =>
+                  birbsMapFr[birbId] === input
+                }
                 renderInput={(params) => (
                   <TextField
                     {...params}
@@ -282,11 +321,10 @@ function App() {
                     variant="outlined"
                   />
                 )}
-                open={birbInput.length > 2}
                 popupIcon={null}
               />
 
-              {selectedBirbs.length > 0 && (
+              {selectedBirbIds.length > 0 && (
                 <Box
                   sx={{
                     maxHeight: "100%",
@@ -298,12 +336,12 @@ function App() {
                       "repeat(auto-fill, minmax(200px, 1fr))",
                   }}
                 >
-                  {selectedBirbs.map((birb, i) => (
+                  {selectedBirbIds.map((birbId, i) => (
                     <Chip
                       key={`chip-${i}`}
-                      label={birb}
+                      label={birbsMapFr[birbId]}
                       variant="outlined"
-                      onDelete={() => deleteBirb(birb)}
+                      onDelete={() => deleteBirb(birbId)}
                     />
                   ))}
                 </Box>
@@ -319,14 +357,14 @@ function App() {
                 <Button
                   variant="outlined"
                   onClick={copyUrl}
-                  disabled={selectedBirbs.length <= 0}
+                  disabled={selectedBirbIds.length <= 0}
                 >
                   Partager
                 </Button>
                 <Button
                   variant="contained"
                   onClick={startQuiz}
-                  disabled={selectedBirbs.length <= 0}
+                  disabled={selectedBirbIds.length <= 0}
                 >
                   Quiz moi
                 </Button>
@@ -365,7 +403,7 @@ function App() {
           {quizStarted && (
             <>
               <LinearProgressWithLabel
-                value={((counter + 1) / selectedBirbs.length) * 100}
+                value={((counter + 1) / selectedBirbIds.length) * 100}
               />
 
               <Box
@@ -391,20 +429,24 @@ function App() {
                   onChange={() => setShowAnswer(!showAnswer)}
                 />
                 <Typography variant="body1">
-                  {showAnswer ? selectedBirbs[sequence![counter]] : "???"}
+                  {showAnswer
+                    ? birbsMapFr[selectedBirbIds[sequence![counter]]]
+                    : "???"}
                 </Typography>
               </Box>
 
               <Button
                 variant={
-                  counter === selectedBirbs.length - 1
+                  counter === selectedBirbIds.length - 1
                     ? "contained"
                     : "outlined"
                 }
                 onClick={nextQuestion}
                 disabled={!showAnswer}
               >
-                {counter === selectedBirbs.length - 1 ? "Terminer" : "Prochain"}
+                {counter === selectedBirbIds.length - 1
+                  ? "Terminer"
+                  : "Prochain"}
               </Button>
             </>
           )}
