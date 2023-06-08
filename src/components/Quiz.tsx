ed6e6b7bc6c73db91bdaa6aacaf0084e8a7c4648
xicshen <xicshen@expediagroup.com>
2023-06-07 20:59:18 -0400

    wip

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,13 +1,10 @@
-import React, { useCallback, useEffect } from "react";
+import React from "react";
 import Button from "@mui/material/Button";
 import {
-  Autocomplete,
   Box,
-  Chip,
   IconButton,
   Switch,
   Tab,
-  TextField,
   Tooltip,
   TooltipProps,
   Typography,
@@ -15,6 +12,8 @@ import {
   tooltipClasses,
 } from "@mui/material";
 import { TabContext, TabList, TabPanel } from "@mui/lab";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 const HtmlTooltip = styled(({ className, ...props }: TooltipProps) => (
   <Tooltip placement="left" {...props} classes={{ popper: className }} />
@@ -53,8 +52,6 @@ function Quiz({
   endQuiz,
   setCounter,
 }: QuizProps) {
-  const [birbInput, setBirbInput] = React.useState<string>("");
-  const [selectedBirbId, setSelectedBirbId] = React.useState<string>("");
   const [tab, setTab] = React.useState(TabName.Songs);
   const [showAnswer, setShowAnswer] = React.useState(false);
 
@@ -63,18 +60,12 @@ function Quiz({
   };
 
   const nextQuestion = () => {
-    if (counter === selectedBirbIds.length - 1) {
-      endQuiz();
-    } else {
-      setCounter(counter + 1);
-      setShowAnswer(false);
-    }
+    setCounter(counter + 1);
+    setShowAnswer(false);
   };
 
   const previousQuestion = () => {
     setCounter(counter - 1);
-    setBirbInput("");
-    setSelectedBirbId("");
     setShowAnswer(false);
   };
 
@@ -126,7 +117,15 @@ function Quiz({
 
   return (
     <>
-      <Box sx={{ display: "flex", justifyContent: "center" }}>
+      <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+        <IconButton
+          color="primary"
+          disabled={counter <= 0}
+          onClick={previousQuestion}
+        >
+          <ArrowBackIcon />
+        </IconButton>
+
         <Typography variant="h4">
           {/* <Box component="span" sx={{ color: "primary.main" }}> */}
           {/* Birbsquizzing... */}
@@ -134,6 +133,14 @@ function Quiz({
           {` ${counter + 1}/${selectedBirbIds.length}`}
           {birbEmoji}
         </Typography>
+
+        <IconButton
+          color="primary"
+          disabled={counter >= selectedBirbIds.length - 1}
+          onClick={nextQuestion}
+        >
+          <ArrowForwardIcon />
+        </IconButton>
       </Box>
 
       <TabContext value={tab}>
@@ -162,6 +169,7 @@ function Quiz({
         {tab === TabName.Photo && (
           <TabPanel
             sx={{
+              marginTop: "1.5rem",
               padding: "0rem 1.5rem",
               overflow: "auto",
               display: "grid",
@@ -206,6 +214,7 @@ function Quiz({
           display: "grid",
           alignItems: "center",
           gridTemplateColumns: "auto 1fr",
+          gap: "0.5rem",
         }}
       >
         <Switch
@@ -217,67 +226,30 @@ function Quiz({
         </Typography>
       </Box>
 
-      <Autocomplete
-        disabled={showAnswer}
-        sx={{ marginTop: "1rem" }}
-        size="small"
-        inputValue={birbInput}
-        onInputChange={(e, v) => setBirbInput(v)}
-        value={selectedBirbId}
-        onChange={(e, v) => setSelectedBirbId(v!)}
-        options={Object.keys(birbsMapFr).sort((a, b) =>
-          birbsMapFr[a].localeCompare(birbsMapFr[b])
-        )}
-        getOptionLabel={(birbId) =>
-          birbsMapFr[birbId] ? birbsMapFr[birbId] : ""
-        }
-        freeSolo
-        isOptionEqualToValue={(birbId, input) => birbsMapFr[birbId] === input}
-        renderInput={(params) => (
-          <TextField {...params} label="Répondre ..." variant="outlined" />
-        )}
-      />
-
       <Box
         sx={{
           marginTop: "1rem",
           display: "grid",
-          gap: "0.5rem",
-          gridTemplateColumns: "1fr",
+          gap: "1rem",
+          gridTemplateColumns: "1fr 1fr",
         }}
       >
-        <Box
-          sx={{
-            display: "grid",
-            gap: "1rem",
-            gridTemplateColumns: "1fr 1fr",
-          }}
+        <Button
+          variant="outlined"
+          disabled={!showAnswer}
+          onClick={previousQuestion}
+          color="error"
         >
-          <Button
-            variant="outlined"
-            disabled={counter <= 0}
-            onClick={previousQuestion}
-          >
-            Dernier
-          </Button>
+          Faux
+        </Button>
 
-          <Button
-            variant={
-              counter === selectedBirbIds.length - 1 ? "contained" : "outlined"
-            }
-            onClick={nextQuestion}
-          >
-            {counter === selectedBirbIds.length - 1 ? "Terminer " : "Prochain "}
-          </Button>
-        </Box>
-
-        {/* <Button variant="outlined" color="error" onClick={endQuiz}>
-        retour
-      </Button> */}
-
-        {/* <LinearProgressWithLabel
-          value={((counter + 1) / selectedBirbIds.length) * 100}
-        /> */}
+        <Button
+          variant="outlined"
+          disabled={!showAnswer}
+          onClick={previousQuestion}
+        >
+          Birb
+        </Button>
       </Box>
     </>
   );
